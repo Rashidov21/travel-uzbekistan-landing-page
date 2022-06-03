7d5ff302d15a470af9dd976cc81094c30f645481
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-06-03 06:58:19 +0500

    create django project

---
+++ travel_uz/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for travel_uz project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travel_uz.settings')
+
+application = get_asgi_application()
